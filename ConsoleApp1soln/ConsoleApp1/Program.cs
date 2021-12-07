d6dc255dfd445839de0ab3dcc0569c80aad4ce28
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2021-12-07 11:41:31 +0530

    new program added

---
+++ ConsoleApp1soln/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine("Type of i is {0)", i.GetType());
+          
+
+
+        }
+    }
+}
---
+++ CsharpFirstProgramSolution/CsharpFirstProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CsharpFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstOperand = 15;
+            int secondOperand = 3;
+
+            int addResult = firstOperand + secondOperand;
+            int subResult = firstOperand - secondOperand;
+            int multiplyResult = firstOperand * secondOperand;
+            int divisionResult = firstOperand / secondOperand;
+            Console.WriteLine("Add:-{0} Substract:-{1} Multiply:-{2}, division:-{3}", addResult, subResult, multiplyResult, divisionResult);
+        }
+    }
+}
